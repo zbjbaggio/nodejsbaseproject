080e8214e7a089b79b09090994b747a7111e6ec0
zhangbaojun <jay@4zlink.com>
2016-10-17 10:51:47 +0800

    remove test

--- service/login.js
+++ service/login.js
@@ -5,7 +5,7 @@
 var userDao = require('../dao/user/userDao');
 var redisDao = require('../dao/redis/redisDao');
 var $util = require('../util/util');
-redisDao.saveLoginSession({"id":"2222222","name":"ni da ye!"});
+
 module.exports = {
     login: function (name, password) {
         let user = userDao.queryByName(name);
